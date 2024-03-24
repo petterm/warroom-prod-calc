7fbad46414930e9270f23251fda945d6116baa7c
Petter Magnusson <petter.a.magnusson@gmail.com>
2024-03-24 13:57:40 +0100

    Changed styles to be darker Disable squares without cost Show dimmed cost at 0 amount

--- src/ProductionCalc.tsx
+++ src/ProductionCalc.tsx
@@ -21,16 +21,29 @@ type UnitPurchase = {
     amount: number,
 }
 
+const colors = {
+    green: 'rgb(0 117 0)',
+    yellow: '#ffff00',
+    red: 'rgb(171 55 55)',
+    blue: '#4949fb',
+}
+
+const resources: { [NAME in Resource ]: { color: string } } = {
+    'oil': { color: colors.red },
+    'steel': { color: colors.blue },
+    'osr': { color: colors.yellow },
+}
+
 const units: { [NAME in UnitName]: Unit } = {
-    'INF': { name: 'INF', color: '#ffff00', cost: { oil: 0, steel: 0, osr: 2 }, type: 'LAND' },
-    'ART': { name: 'ART', color: '#4949fb', cost: { oil: 0, steel: 2, osr: 1 }, type: 'LAND' },
-    'ARM': { name: 'ARM', color: '#03ab03', cost: { oil: 1, steel: 2, osr: 1 }, type: 'LAND' },
-    'FTR': { name: 'FTR', color: '#03ab03', cost: { oil: 2, steel: 1, osr: 1 }, type: 'AIR' },
-    'BMR': { name: 'BMR', color: '#d55959', cost: { oil: 2, steel: 2, osr: 1 }, type: 'AIR' },
-    'SS': { name: 'SS', color: '#ffff00', cost: { oil: 1, steel: 2, osr: 1 }, type: 'SEA' },
-    'CR': { name: 'CR', color: '#4949fb', cost: { oil: 2, steel: 3, osr: 2 }, type: 'SEA' },
-    'CV': { name: 'CV', color: '#03ab03', cost: { oil: 4, steel: 3, osr: 3 }, type: 'SEA' },
-    'BB': { name: 'BB', color: '#d55959', cost: { oil: 3, steel: 4, osr: 3 }, type: 'SEA' },
+    'INF': { name: 'INF', color: colors.yellow, cost: { oil: 0, steel: 0, osr: 2 }, type: 'LAND' },
+    'ART': { name: 'ART', color: colors.blue, cost: { oil: 0, steel: 2, osr: 1 }, type: 'LAND' },
+    'ARM': { name: 'ARM', color: colors.green, cost: { oil: 1, steel: 2, osr: 1 }, type: 'LAND' },
+    'FTR': { name: 'FTR', color: colors.green, cost: { oil: 2, steel: 1, osr: 1 }, type: 'AIR' },
+    'BMR': { name: 'BMR', color: colors.red, cost: { oil: 2, steel: 2, osr: 1 }, type: 'AIR' },
+    'SS': { name: 'SS', color: colors.yellow, cost: { oil: 1, steel: 2, osr: 1 }, type: 'SEA' },
+    'CR': { name: 'CR', color: colors.blue, cost: { oil: 2, steel: 3, osr: 2 }, type: 'SEA' },
+    'CV': { name: 'CV', color: colors.green, cost: { oil: 4, steel: 3, osr: 3 }, type: 'SEA' },
+    'BB': { name: 'BB', color: colors.red, cost: { oil: 3, steel: 4, osr: 3 }, type: 'SEA' },
 };
 const unitNames: UnitName[] = Object.keys(units) as UnitName[];
 
@@ -42,15 +55,34 @@ const getTotal = (cart: UnitPurchase[]): Cost => cart.reduce((total, cartUnit) =
     osr: total.osr + (cartUnit.amount * units[cartUnit.unit].cost.osr),
 }), { oil: 0, steel: 0, osr: 0 });
 
-const unitRow = (unitName: UnitName, amount: number, onAdd: () => void, onSubtract: () => void) => {
-    const unit = units[unitName];
-
+// Show cost even with amount is 1 (but will be styled as empty)
+function Cost(props: { type: Resource, cost: number, amount: number }) {
+    const {type, cost, amount} = props;
+    const color = amount ? resources[type].color : '';
+    if (cost === 0) {
+        return (
+            <div className={`unit__cost unit__cost_${type} unit__cost_disabled`}>
+                &nbsp;
+            </div>
+        )
+    }
     return (
-        <div key={unit.name} className={['unit', `unit-${unitName}`, amount ? '' : 'unit-empty'].join(' ')}>
+        <div className={`unit__cost unit__cost_${type}`} style={{color}}>
+            {cost * Math.max(amount, 1)}
+        </div>
+    )
+}
+
+function UnitRow(props: { unitName: UnitName, amount: number, onAdd: () => void, onSubtract: () => void }) {
+    const {unitName, amount, onAdd, onSubtract} = props;
+    const unit = units[unitName];
+    return (
+
+<div key={unit.name} className={['unit', `unit-${unitName}`, amount ? '' : 'unit-empty'].join(' ')}>
             <button className='unit__name' onClick={onAdd} style={{ color: unit.color}}>{unit.name}</button>
-            <div className='unit__cost unit__cost_oil'>{unit.cost.oil * amount}</div>
-            <div className='unit__cost unit__cost_steel'>{unit.cost.steel * amount}</div>
-            <div className='unit__cost unit__cost_osr'>{unit.cost.osr * amount}</div>
+            <Cost type='oil' cost={unit.cost.oil} amount={amount} />
+            <Cost type='steel' cost={unit.cost.steel} amount={amount} />
+            <Cost type='osr' cost={unit.cost.osr} amount={amount} />
             <button className='unit__cost unit__amount' onClick={onSubtract}>{amount}</button>
         </div>
     )
@@ -69,7 +101,9 @@ function ProductionCalc() {
     const total = getTotal(cart);
     return (
         <div className='productionCalc'>
-            {cart.map(({ unit, amount }) => unitRow(unit, amount, () => addUnit(unit), () => subtractUnit(unit)))}
+            {cart.map(({ unit, amount }) => (
+                <UnitRow key={unit} unitName={unit} amount={amount} onAdd={() => addUnit(unit)} onSubtract={() => subtractUnit(unit)} />
+            ))}
             <div className='unit unit-total'>
                 <div className='unit__name'>Total</div>
                 <div className='unit__cost unit__cost_oil'>{total.oil}</div>
